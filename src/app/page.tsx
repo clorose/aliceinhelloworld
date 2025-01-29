f0d2ae8122e63cea892a3eed68a95aa1e6dfe069
clorose <gohan9511@gmail.com>
2025-01-29 15:39:52 +0900

    🐛 Fix: Remove <a> link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
       <p>Welcome to the Home page</p>
       <li>
-        <a href="/post">P0st</a>
+        <Link href={"/blog"}>Hello</Link>
       </li>
     </>
   );
