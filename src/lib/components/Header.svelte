894c22e07f62e019a6e296c9b1a3af47fe38f1fc
clorose <gohan9511@gmail.com>
2025-12-07 22:51:01 +0900

    ✨ feat: add Header component and login page
    - Add Header component with Panda CSS styling
    - Add login page with basic layout
    - Add favicon.svg asset

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { css } from 'styled-system/css';
+</script>
+
+<header
+	class={css({
+		height: '32px',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		px: '4',
+		borderBottom: '1px solid',
+		borderColor: 'gray.200',
+		fontSize: 'xs'
+	})}
+>
+	<div>logo</div>
+	<nav
+		class={css({
+			display: 'flex',
+			alignItems: 'center',
+			gap: '3'
+		})}
+	>
+		<a href="/">홈</a>
+		<a href="/login">로그인</a>
+		<div>1:30</div>
+	</nav>
+</header>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,7 @@
+<title>로그인 페이지</title>
+
+
+<h1>
+  <a href="/">돌아가기</a>
+</h1>
+<button>로그인</button>
